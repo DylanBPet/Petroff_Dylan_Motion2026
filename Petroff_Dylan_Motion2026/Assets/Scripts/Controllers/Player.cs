d3a9aa9df01fa56bf71f4a235480cf8e71864ce9
petrofdy <petrofdy@OA-S142-18>
2026-09-14 19:04:57 -0400

    Journal 2, Task 1, Complete

--- Petroff_Dylan_Motion2026/Assets/Scripts/Controllers/Player.cs
+++ Petroff_Dylan_Motion2026/Assets/Scripts/Controllers/Player.cs
@@ -9,32 +9,61 @@ public class Player : MonoBehaviour
     public Transform enemyTransform;
     public GameObject bombPrefab;
     public Transform bombsTransform;
-    public Vector2 inOffset;
+
+    public Vector3 inOffset;
+
+    public Vector3 bombOffset;
+    private Vector2 playerPos;
+
+    public int numberOfBombs;
 
     void Start()
     {
-        inOffset = (new Vector2(0, 1));
+    
     }
     void Update()
     {
+        playerPos = transform.position;
+
+        if (Keyboard.current.tKey.wasPressedThisFrame)
+        {
+            SpawnBombAtOffset(bombOffset);
+        }
+
         if (Keyboard.current.bKey.wasPressedThisFrame)
         {
-            SpawnBombAtOffset(inOffset);
+            SpawnBombAhead(inOffset);
         }
 
-        if (Keyboard.current.wKey.wasPressedThisFrame)
+    }
+
+    void SpawnBombAhead(Vector3 inOffset)
+    {
+        Vector3 spawnPos = transform.position + inOffset;
+        Instantiate(bombPrefab, spawnPos, Quaternion.identity);
+
+        Debug.Log("player Pos: " + transform.position);
+        Debug.Log("Bomb Spawned at: " + spawnPos);
+        Debug.Log("offset is: " + inOffset);
+    }
+
+    void SpawnBombAtOffset(Vector2 distanceBetweenBombs)
+    {
+
+        for (int i = 1; i < numberOfBombs + 1; i++)
         {
-            MoveSpaceShip(inOffset);
+            Vector2 spaceBetweenBombs = distanceBetweenBombs * i;
+
+            Vector2 spawnPos = playerPos + spaceBetweenBombs;
+
+            Instantiate(bombPrefab, spawnPos, Quaternion.identity);
+
+            Debug.Log("player Pos: " + transform.position);
+            Debug.Log("offset is: " + distanceBetweenBombs);
+            Debug.Log("bomb " + i + " Spawned at " + spawnPos);
+            
         }
+       
     }
 
-    void SpawnBombAtOffset(Vector2 inOffset)
-    {
-        Instantiate(bombPrefab, inOffset, Quaternion.identity);
-    }
-
-    void MoveSpaceShip(Vector2 inOffset)
-    {
-        transform.position += (Vector3)inOffset;
-    }
 }
