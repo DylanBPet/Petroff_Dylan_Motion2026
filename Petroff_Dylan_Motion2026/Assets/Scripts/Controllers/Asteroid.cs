62b1d8557418b1cac7344ee005f1905c765edf5c
DylanBPet <dylanpetroff13@gmail.com>
2026-09-21 23:48:42 -0400

    Journal 3, Task 2 fully implemented

--- Petroff_Dylan_Motion2026/Assets/Scripts/Controllers/Asteroid.cs
+++ Petroff_Dylan_Motion2026/Assets/Scripts/Controllers/Asteroid.cs
@@ -8,6 +8,10 @@ public class Asteroid : MonoBehaviour
     public float arrivalDistance;
     public float maxFloatDistance;
 
+    public Vector3 endPos;
+
+    public bool randomLocationSelected = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,5 +20,28 @@ public class Asteroid : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        AsteroidMovement();
+    }
+
+    public void AsteroidMovement()
+    {
+        if (randomLocationSelected == false)
+        {
+            endPos = (Random.insideUnitCircle * maxFloatDistance) + (Vector2)transform.position;
+            Debug.DrawLine(transform.position, endPos, Color.white, 100f);
+            randomLocationSelected = true;
+        }
+
+        float dist = Vector2.Distance(transform.position, endPos);
+
+        if (arrivalDistance < dist)
+        {
+            Vector3 direction = endPos - transform.position;
+            transform.position += direction.normalized * moveSpeed * Time.deltaTime;
+        }
+        else
+        {
+            randomLocationSelected = false;
+        }
     }
 }
